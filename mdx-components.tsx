dfd51e9664d13fc72fa77be5206ed4345aecfa14
Joohyung Park <johy@dabyeol.com>
2023-11-03 19:02:11 +0900

    Add image optimization for work page

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,4 +1,5 @@
 import type { MDXComponents } from 'mdx/types';
+import Image from 'next/image';
 
 // This file allows you to provide custom React components
 // to be used in MDX files. You can import and use any
@@ -8,9 +9,23 @@ import type { MDXComponents } from 'mdx/types';
 // This file is required to use MDX in `app` directory.
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    // Allows customizing built-in components, e.g. to add styling.
-    // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
-    // img: props => <img {...props} loading="eager" />,
+    img: ({
+      className,
+      src,
+      alt,
+    }: React.ImgHTMLAttributes<HTMLImageElement>) => (
+      <Image
+        className={className}
+        src={src!}
+        width={0}
+        height={0}
+        sizes="100vw"
+        alt={alt!}
+        style={{ width: '100%', height: 'auto' }}
+        priority
+      />
+    ),
+
     ...components,
   };
 }
