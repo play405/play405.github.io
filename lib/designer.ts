3fdd58867ff2dae40d74febeac6a818187ff8fbc
Joohyung Park <johy@dabyeol.com>
2023-11-16 20:53:37 +0900

    Update designer data

--- lib/designer.ts
+++ lib/designer.ts
@@ -16,12 +16,11 @@ export const designers: Designer[] = [
     id: 1,
     name: '임건',
     engName: 'Yim Gun',
-    description: '인스타/비핸스 작업계정\nE. gildong1234@gmail.com',
+    description: 'towduri1127@naver.com',
     cartridge: {
       title: 'MUK2',
       category: 'Product',
-      description:
-        'Lorem ipsum dolor sit amet consectetur. Id aenean scelerisque eros massa risus facilisis mauris tellus. Turpis elementum in sed sed. Justo pharetra viverra mauris neque commodo.',
+      description: `많은 사람들이 인터넷에 토해내는 건강하지 않고 비건설적인 말들의 원인이 다른 사회적인 원인뿐만이 아니라 컴퓨터와 관련한 '물건'에서도 있지 않을까.`,
       color: '#b1cae0',
     },
   },
@@ -29,12 +28,12 @@ export const designers: Designer[] = [
     id: 2,
     name: '이윤미',
     engName: 'Lee Yunmi',
-    description: '인스타/비핸스 작업계정\nE. gildong1234@gmail.com',
+    description: 'lym2467@naver.com',
     cartridge: {
       title: 'Sooan',
       category: 'Product',
-      description:
-        '시각장애인의 필수 생활용구이자 자립과 성취의 상징인 흰지팡이는 그들의 눈이 되어주며 길을 밝혀줍니다. 마음 놓고 활동할 수 있는 권리를 보장해 주는 또 하나의 표시이기도 합니다.\n그러나 현실은 노후된 편의 시설들과 명확하지 않은 점자 안내판으로 인해 정확한 정보 전달이 되지 않아 그들의 안전 또한 위협받습니다. 시각장애인이 진정 마음 놓고 안전한 보행을 할 수 있도록 돕기 위해 더욱 효율적이고 스마트한 흰지팡이 ‘수안(Sooan)’을 제시합니다.',
+      description: `시각장애인의 필수 생활용구이자 자립과 성취의 상징인 흰지팡이는 그들의 눈이 되어주며 길을 밝혀줍니다. 마음 놓고 활동할 수 있는 권리를 보장해 주는 또 하나의 표시이기도 합니다.
+그러나 현실은 노후된 편의 시설들과 명확하지 않은 점자 안내판으로 인해 정확한 정보 전달이 되지 않아 그들의 안전 또한 위협받습니다. 시각장애인이 진정 마음 놓고 안전한 보행을 할 수 있도록 돕기 위해 더욱 효율적이고 스마트한 흰지팡이 ‘수안(Sooan)’을 제시합니다.`,
       color: '#797979',
     },
   },
@@ -42,12 +41,12 @@ export const designers: Designer[] = [
     id: 3,
     name: '이진희',
     engName: 'Lee Jinhee',
-    description: '인스타/비핸스 작업계정\nE. gildong1234@gmail.com',
+    description: 'ndlwlsgml0506@naver.com',
     cartridge: {
       title: 'W.FURNITURE',
       category: 'Product',
-      description:
-        '회사와 집에서 업무, 자기개발을 위해 하루 대부분의 시간을 앉아서 보내는 현대인들을 위한 앉고 서기가 가능한 2WAY 독서대이다.\n합리적인 가격으로 자신의 신장에 맞춰 한정된 공간을 효율적으로 사용하게끔 자취생들을 위한 가구를 만들고 싶었다. 또한, 원목가구를 제작하며 플랜테리어 분야에서 있는 본인은 다른 소비자들도 자연 가까이서 일상생활을 누리길 원하는 마음을 담았다.',
+      description: `회사와 집에서 업무, 자기개발을 위해 하루 대부분의 시간을 앉아서 보내는 현대인들을 위한 앉고 서기가 가능한 2WAY 독서대이다.
+합리적인 가격으로 자신의 신장에 맞춰 한정된 공간을 효율적으로 사용하게끔 자취생들을 위한 가구를 만들고 싶었다. 또한, 원목가구를 제작하며 플랜테리어 분야에서 있는 본인은 다른 소비자들도 자연 가까이서 일상생활을 누리길 원하는 마음을 담았다.`,
       color: '#8abc69',
     },
   },
@@ -55,25 +54,25 @@ export const designers: Designer[] = [
     id: 4,
     name: '김정재',
     engName: 'Kim Jeoungjae',
-    description: '@user_32038501',
+    description: 'hiroo1734@gmail.com\n@user_32038501',
     cartridge: {
       title: '파괴자',
       category: 'Design',
-      description:
-        '우리는 대체 불가능하다고 여겨왔던 모든 분야에서 AI가 인간 개인을 앞서는 시대에 버려졌다.',
-      color: '#ff0000',
+      description: `우리는 대체 불가능하다고 여겨왔던 모든 분야에서 AI가 인간 개인을 앞서는 시대에 버려졌다.`,
+      color: '#914',
     },
   },
   {
     id: 5,
     name: '구가영',
     engName: 'Koo Gayeong',
-    description: '인스타/비핸스 작업계정\nE. gildong1234@gmail.com',
+    description: 'kooo08762@gmail.com',
     cartridge: {
       title: '약알',
       category: 'UX/UI',
-      description:
-        'Lorem ipsum dolor sit amet consectetur. Id aenean scelerisque eros massa risus facilisis mauris tellus. Turpis elementum in sed sed. Justo pharetra viverra mauris neque commodo.',
+      description: `"약을 올바르게 알고 복용해요!"
+'약알'은 당신과 가장 가까운 이웃 약사이자, 다제약물 문제를 해결해주는 건강 메이트이기도 해요. 다제약물이란 여러 가지 약을 동시에 복용하는 것인데, 이는 특히 고령자들에게 큰 부작용으로 다가올 수 있어서 조심해야 해요!
+모든 사용자가 약에 대해 정확히 알고 올바르게 복용할 수 있도록 돕는 것이 우리의 목표예요. '약알'이라는 이름처럼, ‘약’에 대한 모든 것을 '알'려주는 믿음직한 복약도우미가 되어 드릴게요 :)`,
       color: '#2666f6',
     },
   },
@@ -81,12 +80,12 @@ export const designers: Designer[] = [
     id: 6,
     name: '김혜진',
     engName: 'Kim Hyejin',
-    description: '인스타/비핸스 작업계정\nE. gildong1234@gmail.com',
+    description: 'jin102745@gmail.com',
     cartridge: {
       title: '테잇',
       category: 'UX/UI',
-      description:
-        '테잇은 ‘해외여행 갈 때 쉽고 빠르게 맛집을 찾을 순 없을까?’ 라는 생각으로 시작된 맞춤형 해외 맛집 큐레이션 서비스입니다.\n해외 맛집을 검색할 때 원하는 정보를 찾기 어렵거나, 많은 검색 정보량 중 바이럴 또는 관광객 맛집으로 신뢰성이 떨어지는 경우가 흔했습니다. 테잇은 이런 문제점을 개선하여 사용자가 여행지를 선택하면 해당 지역에 맞춘 맛집 및 현지 가이드를 제공하고 6가지 평가 기준을 기반으로 한 상세 리뷰와 같은 맞춤형 서비스를 확인할 수 있습니다.',
+      description: `테잇은 ‘해외여행 갈 때 쉽고 빠르게 맛집을 찾을 순 없을까?’ 라는 생각으로 시작된 맞춤형 해외 맛집 큐레이션 서비스입니다.
+해외 맛집을 검색할 때 원하는 정보를 찾기 어렵거나, 많은 검색 정보량 중 바이럴 또는 관광객 맛집으로 신뢰성이 떨어지는 경우가 흔했습니다. 테잇은 이런 문제점을 개선하여 사용자가 여행지를 선택하면 해당 지역에 맞춘 맛집 및 현지 가이드를 제공하고 6가지 평가 기준을 기반으로 한 상세 리뷰와 같은 맞춤형 서비스를 확인할 수 있습니다.`,
       color: '#e94e1a',
     },
   },
@@ -96,10 +95,10 @@ export const designers: Designer[] = [
     engName: 'Park Joohyung',
     description: 'johy@dabyeol.com',
     cartridge: {
-      title: "Dino's",
+      title: 'Dino’s',
       category: 'UX/UI',
-      description:
-        '재택 근무자와 프리랜서처럼, 일을 할 때 공간의 제약을 받지 않는 인구 수가 늘어나고 있다. 이러한 추세에 따라 디지털 노마드의 수 또한 증가하였으며 디지털 노마드에 관심을 가지는 사람도 늘어났다.\n새로 유입되는 디지털 노마드가 많은 데에 비해 이들에게 필요한 정보는 곳곳에 숨겨져 있다. 특히 자신의 성향에 맞는 정보를 얻기까지 많은 시간을 쏟아야 하는 문제점이 있다. 이들에게 도움을 주기 위한 서비스, ‘디노스’를 제안한다.',
+      description: `재택 근무자와 프리랜서처럼, 일을 할 때 공간의 제약을 받지 않는 인구 수가 늘어나고 있다. 이러한 추세에 따라 디지털 노마드의 수 또한 증가하였으며 디지털 노마드에 관심을 가지는 사람도 늘어났다.
+새로 유입되는 디지털 노마드가 많은 데에 비해 이들에게 필요한 정보는 곳곳에 숨겨져 있다. 특히 자신의 성향에 맞는 정보를 얻기까지 많은 시간을 쏟아야 하는 문제점이 있다. 이들에게 도움을 주기 위한 서비스, ‘디노스’를 제안한다.`,
       color: '#bb9977',
     },
   },
@@ -107,12 +106,12 @@ export const designers: Designer[] = [
     id: 8,
     name: '이은아',
     engName: 'Lee Euna',
-    description: '인스타/비핸스 작업계정\nE. gildong1234@gmail.com',
+    description: 'lea0116@naver.com',
     cartridge: {
       title: 'Memory.of',
       category: 'UX/UI',
-      description:
-        "지나온 나만의 추억을 떠올리며 행복의 감정을 경험해본적이 있으신가요?\n길을 걷고 노래를 듣다 문득 떠오르는 추억은 우리의 마음에 작은 꽃을 피워줍니다. 그리고 그 꽃은 자라나 활짝 피어주겠죠. 빠르게 바뀌는 오늘날의 흐름 속에서 여기, 나만의 이야기에 더욱 귀기울이고 싶은 사람들을 위한 서비스 'Memory.of'를 소개합니다.",
+      description: `지나온 나만의 추억을 떠올리며 행복의 감정을 경험해본적이 있으신가요?
+길을 걷고 노래를 듣다 문득 떠오르는 추억은 우리의 마음에 작은 꽃을 피워줍니다. 그리고 그 꽃은 자라나 활짝 피어주겠죠. 빠르게 바뀌는 오늘날의 흐름 속에서 여기, 나만의 이야기에 더욱 귀기울이고 싶은 사람들을 위한 서비스 'Memory.of'를 소개합니다.`,
       color: '#cad3a2',
     },
   },
@@ -120,12 +119,13 @@ export const designers: Designer[] = [
     id: 9,
     name: '윤솔비',
     engName: 'Yun Solbi',
-    description: '인스타/비핸스 작업계정\nE. gildong1234@gmail.com',
+    description: 'thfql0427@naver.com',
     cartridge: {
       title: '기억의 정원',
       category: 'Service',
-      description:
-        'Lorem ipsum dolor sit amet consectetur. Id aenean scelerisque eros massa risus facilisis mauris tellus. Turpis elementum in sed sed. Justo pharetra viverra mauris neque commodo.',
+      description: `반려동물을 무지개다리 너머로 보내준 경험이 있는가?
+현시점의 반려동물 장례는 의미도 모른 채 지도사가 이끄는 대로 행해지는 장례 절차, 과도한 장례비용, 사람의 관점에서 이루어지는 장례로 인해 보호자와 반려동물을 배려하지 못한 형태로 자리 잡고 있다. 사치 서비스가 아닌 가치 서비스를 제공하면서 아픈 이별의 슬픔보다는 따뜻한 마지막 인사가 될 수 있도록 
+새로운 장례 문화를 장려하고자 한다.`,
       color: '#faf8e9',
     },
   },
@@ -133,12 +133,11 @@ export const designers: Designer[] = [
     id: 10,
     name: '김지언',
     engName: 'Kim Jieon',
-    description: '인스타/비핸스 작업계정\nE. gildong1234@gmail.com',
+    description: 'wldjs2967@gmail.com',
     cartridge: {
       title: 'New culture',
       category: 'Motion',
-      description:
-        '새로운 환경을 마주하면 사람들은 어떤 감정을 가질까? 문화 충격은 익숙하지 않은 환경에 있을 때 나타날 수 있으며 새로운 문화를 소화하는 데 어려움을 겪을 수도, 무엇이 옳고 그른지를 알기 어려워질 수 있다. 하지만 새로운 사람들을 만나 다양한 감정의 변화 단계를 적응해 나아간다. 이 과정을 컬러가 가진 감정의 의미와 변화, 혼란스럽고 불편함에 동조하여 스스로가 무너질 것 같아도 다시 이겨내는 모습을 시각적으로 표현하고자 한다.',
+      description: `새로운 환경을 마주하면 사람들은 어떤 감정을 가질까? 문화 충격은 익숙하지 않은 환경에 있을 때 나타날 수 있으며 새로운 문화를 소화하는 데 어려움을 겪을 수도, 무엇이 옳고 그른지를 알기 어려워질 수 있다. 하지만 새로운 사람들을 만나 다양한 감정의 변화 단계를 적응해 나아간다. 이 과정을 컬러가 가진 감정의 의미와 변화, 혼란스럽고 불편함에 동조하여 스스로가 무너질 것 같아도 다시 이겨내는 모습을 시각적으로 표현하고자 한다.`,
       color: '#ff89ac',
     },
   },
@@ -146,12 +145,11 @@ export const designers: Designer[] = [
     id: 11,
     name: '김플잎',
     engName: 'Kim Peulyip',
-    description: '인스타/비핸스 작업계정\nE. gildong1234@gmail.com',
+    description: 'py_637@naver.com',
     cartridge: {
       title: 'EGO 24 : 조선',
       category: 'Motion',
-      description:
-        '이 세상의 모든 것은 순간의 선택과 갈림길에 따라 각기 다른 모습을 갖게 된다. [EGO 42;조선]은 바로 그런 갖은 선택에 의해 다른 모습이 되어버린 우리나라의 과거, 현재, 미래 중 하나, 또는 모든 것을 표현한 작품이다. 그럼에도 불구하고, 본 작품에서는 그 모습이 우리나라로 보일 수 있도록 우리 민족이 공유하고 있는 요소를 찾아내고 표현하고자 한다.',
+      description: `이 세상의 모든 것은 순간의 선택과 갈림길에 따라 각기 다른 모습을 갖게 된다. [EGO 42;조선]은 바로 그런 갖은 선택에 의해 다른 모습이 되어버린 우리나라의 과거, 현재, 미래 중 하나, 또는 모든 것을 표현한 작품이다. 그럼에도 불구하고, 본 작품에서는 그 모습이 우리나라로 보일 수 있도록 우리 민족이 공유하고 있는 요소를 찾아내고 표현하고자 한다.`,
       color: '#1d1d1d',
     },
   },
@@ -159,12 +157,16 @@ export const designers: Designer[] = [
     id: 12,
     name: '양영하',
     engName: 'Yang Youngha',
-    description: '인스타/비핸스 작업계정\nE. gildong1234@gmail.com',
+    description: 'coco2970@naver.com',
     cartridge: {
       title: '호접지몽',
       category: 'Motion',
-      description:
-        "'모든 꿈에는 의미가 있다'\n\n꿈은 여러분들에게 어떤 의미를 가져다 주는가\n사람들은 '꿈'이 어떤 의미를 가지고 있는지 해석을 하는데 초점을 맞추기보다,\n행운을 가져다 주는 꿈인지, 불행을 가져다 주는 꿈인지에 대해서만 많은 관심을 가지고 있다.\n'꿈'이 나타나는 표상적인 이미지(숨어있는 욕망)에 대해서 한번쯤은 고민해보고 생각해보는 시간을 가진다면, 나도 모르는 나의 내면세계에 한발짝 다가갈수 있다고 생각한다.\n단편 애니메이션 <The butterfly Dream>을 통해 꿈이 가지고 있는 긍정적인 힘에 대해서 이야기할 수 있는 시간이 되기를 희망한다.",
+      description: `'모든 꿈에는 의미가 있다'
+꿈은 여러분들에게 어떤 의미를 가져다 주는가
+사람들은 '꿈'이 어떤 의미를 가지고 있는지 해석을 하는데 초점을 맞추기보다,
+행운을 가져다 주는 꿈인지, 불행을 가져다 주는 꿈인지에 대해서만 많은 관심을 가지고 있다.
+'꿈'이 나타나는 표상적인 이미지(숨어있는 욕망)에 대해서 한번쯤은 고민해보고 생각해보는 시간을 가진다면, 나도 모르는 나의 내면세계에 한발짝 다가갈수 있다고 생각한다.
+단편 애니메이션 <The butterfly Dream>을 통해 꿈이 가지고 있는 긍정적인 힘에 대해서 이야기할 수 있는 시간이 되기를 희망한다.`,
       color: '#eca4af',
     },
   },
